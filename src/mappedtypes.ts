073edc025366092572d9d379062d4092035e874e
mofazzal-hossen <mofazzal.hossen.official0@gmail.com>
2026-05-05 11:22:42 +0600

    explore useing map

---
+++ src/mappedtypes.ts
@@ -0,0 +1,12 @@
+//   Explore mapped types
+
+// map 
+
+const arrayOfNum: number[]= [1,2,3,];
+
+const arrayOfString: string[]= ["2",'5',"6"];
+
+
+const arrayOfStringUsingMap: string[]= arrayOfNum.map((num)=>num,toString());
+
+
